5f791577d29dc3a360970e9ae899561ac4558933
kharadimihir <kharadimihir739@gmail.com>
2025-06-05 16:22:06 +0530

    issue solver

--- server/server.js
+++ server/server.js
@@ -30,6 +30,8 @@ app.use("/api/clerk", clerkWebhooks);
 app.get("/", (req, res) => {
     res.send("App is working very very fine");
 });
+
+app.get("/favicon.ico", (req, res) => res.status(204).end());
 app.use("/api/user", userRouter);
 app.use("/api/hotels", hotelRouter);
 app.use("/api/rooms", roomRouter);
